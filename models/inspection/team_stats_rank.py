2fb1e748164b999746b6b1f9327fc151a14c4280
yunjongyun <yunyami0605@gmail.com>
2025-08-02 20:20:52 +0900

    fix: 팀 순위 반환 로직 변경
    1. import 수정

--- models/inspection/team_stats_rank.py
+++ models/inspection/team_stats_rank.py
@@ -1,7 +1,8 @@
 
 
-from matplotlib.font_manager import json_load
 import pandas as pd
+
+from libs.json import load_from_json
 
 
 def get_team_stats_rank(stat_type = "defense"):
@@ -9,7 +10,7 @@ def get_team_stats_rank(stat_type = "defense"):
         stat_type 에 따른 순위 반환
     '''
 
-    defense_team_raw_data = json_load(f"data/rank_spectators/kbo_team_{stat_type}_basic.json")
+    defense_team_raw_data = load_from_json(f"data/rank_spectators/kbo_team_{stat_type}_basic.json")
         
     # DataFrame으로 변환
     defense_team_data = pd.DataFrame(defense_team_raw_data)
